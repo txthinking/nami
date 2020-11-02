d878bcc9029bf399e0b45ff4c233e1afc03ce0ae
txthinking <cloud@txthinking.com>
2020-11-02 05:48:37 +0000

    make better for nami install nami

--- install.sh
+++ install.sh
@@ -6,65 +6,67 @@ os=""
 arch=""
 
 if [ $(uname -s) = "Darwin" ]; then
-	os="darwin"
+    os="darwin"
 fi
 if [ $(uname -s) = "FreeBSD" ]; then
-	os="freebsd"
+    os="freebsd"
 fi
 if [ $(uname -s) = "Linux" ]; then
-	os="linux"
+    os="linux"
 fi
 if [ $(uname -s) = "NetBSD" ]; then
-	os="netbsd"
+    os="netbsd"
 fi
 if [ $(uname -s) = "OpenBSD" ]; then
-	os="openbsd"
+    os="openbsd"
 fi
 if [ $(uname -s | grep -P "MINGW" | wc -l) -eq 1 ]; then
-	os="windows"
+    os="windows"
 fi
 
 if [ $(uname -m) = "x86_64" ]; then
-	arch="amd64"
+    arch="amd64"
 fi
 if [ $(uname -m) = "i386" ]; then
-	arch="386"
+    arch="386"
 fi
 if [ $(uname -m) = "i686" ]; then
-	arch="386"
+    arch="386"
 fi
 if [ $(uname -m) = "arm64" ]; then
-	arch="arm64"
+    arch="arm64"
 fi
 if [ $(uname -m) = "aarch64" ]; then
-	arch="arm64"
+    arch="arm64"
 fi
 
 if [ "$os" = "" -o "$arch" = "" ]; then
-	echo "This script does not support your OS/ARCH yet. Please submit issue or PR to https://github.com/txthinking/nami"
-	exit
+    echo "This script does not support your OS/ARCH yet. Please submit issue or PR to https://github.com/txthinking/nami"
+    exit
 fi
 
 sfx=""
 if [ $os = "windows" ]; then
-	sfx=".exe"
+    sfx=".exe"
 fi
 
-curl -L -o /tmp/nami0$sfx "https://github.com/txthinking/nami/releases/download/$version/nami_${os}_${arch}$sfx"
-chmod +x /tmp/nami0$sfx
-/tmp/nami0$sfx install github.com/txthinking/nami
+t=$(date +%s)
+
+curl -L -o /tmp/nami${t}$sfx "https://github.com/txthinking/nami/releases/download/$version/nami_${os}_${arch}$sfx"
+chmod +x /tmp/nami${t}$sfx
+/tmp/nami${t}$sfx install github.com/txthinking/nami
 
 rc="$HOME/.bashrc"
 if [ "$os" = "darwin" ]; then
-	rc="$HOME/.bash_profile"
+    rc="$HOME/.bash_profile"
 fi
 echo 'if [ -d $HOME/.nami/bin ]; then' >>$rc
 echo '    export PATH=$HOME/.nami/bin:$PATH' >>$rc
 echo 'fi' >>$rc
 
 if [ "$os" = "darwin" ]; then
-	echo source ~/.bash_profile >>$HOME/.zshenv
+    echo source ~/.bash_profile >>$HOME/.zshenv
 fi
 if [ "$os" != "darwin" ]; then
-	echo source ~/.bashrc >>$HOME/.zshenv
+    echo source ~/.bashrc >>$HOME/.zshenv
 fi
--- nami.go
+++ nami.go
@@ -25,7 +25,9 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/olekukonko/tablewriter"
 	"go.etcd.io/bbolt"
@@ -97,6 +99,7 @@ func (n *Nami) Install(name string) (func(), error) {
 	if len(m) == 0 {
 		return nil, errors.New(fmt.Sprintf("No files for %s %s", runtime.GOOS, runtime.GOARCH))
 	}
+	t := strconv.FormatInt(time.Now().Unix(), 10)
 	for k, v := range m {
 		if (k == "nami" || k == "nami.exe") && name != "github.com/txthinking/nami" {
 			log.Println("Ignore nami binary from package", name)
@@ -113,7 +116,7 @@ func (n *Nami) Install(name string) (func(), error) {
 		r.Body.Close()
 		s := filepath.Join(n.BinDir, k)
 		if name == "github.com/txthinking/nami" {
-			s = filepath.Join(os.TempDir(), k)
+			s = filepath.Join(os.TempDir(), k+t)
 		}
 		if err := ioutil.WriteFile(s, b, 0755); err != nil {
 			return nil, err
@@ -144,9 +147,9 @@ func (n *Nami) Install(name string) (func(), error) {
 	}
 	if name == "github.com/txthinking/nami" {
 		return func() {
-			cmd := exec.Command("sh", "-c", "sleep 3 && cp "+filepath.Join(os.TempDir(), "nami")+" "+filepath.Join(n.BinDir, "nami"))
+			cmd := exec.Command("sh", "-c", "sleep 3 && cp "+filepath.Join(os.TempDir(), "nami"+t)+" "+filepath.Join(n.BinDir, "nami"))
 			if runtime.GOOS == "windows" {
-				cmd = exec.Command("cmd", "/C", fmt.Sprintf("ping localhost -n 3 -w 1000 > NUL && copy /y %s %s", filepath.Join(os.TempDir(), "nami.exe"), filepath.Join(n.BinDir, "nami.exe")))
+				cmd = exec.Command("cmd", "/C", fmt.Sprintf("ping localhost -n 3 -w 1000 > NUL && copy /y %s %s", filepath.Join(os.TempDir(), "nami.exe"+t), filepath.Join(n.BinDir, "nami.exe")))
 			}
 			if err := cmd.Start(); err != nil {
 				log.Println(err)
