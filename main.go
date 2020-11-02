650b76d4897be319b935a8b99787d6ca4b7857df
txthinking <cloud@txthinking.com>
2020-11-02 07:23:11 +0000

    v20201101

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "nami"
-	app.Version = "20200516"
+	app.Version = "20201101"
 	app.Usage = "A decentralized binary package manager"
 	app.Authors = []*cli.Author{
 		{
