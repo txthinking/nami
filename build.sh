737e32458e237222cca03b372933c891f749f3a8
TxThinking <cloud@txthinking.com>
2022-01-12 21:25:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,24 +12,28 @@ unzip deno.zip
 mv deno static/
 rm deno.zip
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-w -s" -o _/nami_darwin_arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-w -s" -o _/nami_Darwin_arm64
 
 curl -L https://github.com/denoland/deno/releases/latest/download/deno-x86_64-apple-darwin.zip -o deno.zip
 unzip deno.zip
 mv deno static/
 rm deno.zip
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_darwin_amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_Darwin_x86_64
 
 curl -L https://github.com/denoland/deno/releases/latest/download/deno-x86_64-unknown-linux-gnu.zip -o deno.zip
 unzip deno.zip
 mv deno static/
 rm deno.zip
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_linux_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_Linux_x86_64
 
 curl -L https://github.com/denoland/deno/releases/latest/download/deno-x86_64-pc-windows-msvc.zip -o deno.zip
 unzip deno.zip
 mv deno.exe static/
 rm deno.zip
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_windows_amd64.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_MINGW64_x86_64.exe
 
 nami release github.com/txthinking/nami $1 _
 
