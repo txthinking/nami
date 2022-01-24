e43dc536932f4410fa9a73b39e7dd11fb88908fb
TxThinking <cloud@txthinking.com>
2022-01-24 13:36:49 +0800

    Update mdtoc.js

--- package/mdtoc.js
+++ package/mdtoc.js
@@ -5,6 +5,9 @@ n.open_sourced_on("https://gist.github.com/txthinking/b8915f0b15a761833bc89ee9a6
 
 await n.version("1");
 
+console.log("$ mdtoc xxx.md")
+console.log("$ mdtoc1d xxx.md")
+
 if(Deno.build.os == "darwin" && Deno.build.arch == "aarch64"){
     await n.download_command_from_url("https://gist.githubusercontent.com/txthinking/b8915f0b15a761833bc89ee9a60bbda1/raw/f967f7565578ad64c172eeaa030f367db2a55f06/mdtoc", "mdtoc");
     await n.download_command_from_url("https://gist.githubusercontent.com/txthinking/b8915f0b15a761833bc89ee9a60bbda1/raw/f967f7565578ad64c172eeaa030f367db2a55f06/mdtoc1d", "mdtoc1d");
