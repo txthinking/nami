a6118d53cf5a87afd5a6261d2acf38215fc7f5a2
TxThinking <cloud@txthinking.com>
2022-02-25 12:15:51 +0800

    Update prettier.js

--- package/prettier.js
+++ package/prettier.js
@@ -7,19 +7,19 @@ n.open_sourced_on("https://github.com/prettier/prettier");
 await n.version('unknown');
 
 if(Deno.build.os == "darwin" && Deno.build.arch == "aarch64"){
-    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode("npx --yes prettier"));
+    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode('sh -c "npx --yes prettier $*"'));
     Deno.exit(0);
 }
 if(Deno.build.os == "darwin" && Deno.build.arch == "x86_64"){
-    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode("npx --yes prettier"));
+    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode('sh -c "npx --yes prettier $*"'));
     Deno.exit(0);
 }
 if(Deno.build.os == "linux" && Deno.build.arch == "x86_64"){
-    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode("npx --yes prettier"));
+    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode('sh -c "npx --yes prettier $*"'));
     Deno.exit(0);
 }
 if(Deno.build.os == "windows" && Deno.build.arch == "x86_64"){
-    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode("npx --yes prettier"));
+    await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "prettier")}`, new TextEncoder().encode('sh -c "npx --yes prettier $*"'));
     Deno.exit(0);
 }
 n.unsupport();
