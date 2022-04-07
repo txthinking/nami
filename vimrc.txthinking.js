7b3a6945dfc9d1f47250f0b41fbd6b5d29e62f84
TxThinking <cloud@txthinking.com>
2022-04-07 16:50:00 +0800

    Create vimrc.txthinking.js

---
+++ vimrc.txthinking.js
@@ -0,0 +1,19 @@
+import nami from 'https://raw.githubusercontent.com/txthinking/nami/master/nami.js';
+import {sh1, sh, s2b, home, echo} from 'https://raw.githubusercontent.com/txthinking/denolib/master/f.js';
+
+var n = nami("vimrc.txthinking");
+n.open_sourced_on("https://github.com/txthinking/dotfiles");
+
+var r = await n.fetch("https://raw.githubusercontent.com/txthinking/dotfiles/master/vim/.vimrc");
+var t = await r.text();
+await Deno.writeFile(`${home(".vimrc")}`, s2b(t));
+if (Deno.build.os == "linux") {
+  await sh(`sudo apt-get -y install make gcc g++ python-dev cmake vim-nox git build-essential python3-dev silversearcher-ag`);
+}
+await sh(`rm -rf ${home(".vim")}`);
+await sh(`mkdir ${home(".vim")}`);
+await sh(`git clone https://github.com/gmarik/Vundle.vim.git ${home(".vim", "bundle", "Vundle.vim")}`);
+await sh(`rm -rf ${home(".fzf")}`);
+await sh(`git clone --depth 1 https://github.com/junegunn/fzf.git ${home(".fzf")}`);
+await sh(`vim +PluginInstall +qall`);
+await sh(`vim +GoInstallBinaries +qall`);
