bf9c5c5c720175d9936451802802c2870470ea84
TxThinking <cloud@txthinking.com>
2022-04-13 16:56:21 +0800

    Update and rename ipio.js to tun2brook.js

--- package/tun2brook.js
+++ package/tun2brook.js
@@ -1,27 +1,27 @@
 import nami from "https://raw.githubusercontent.com/txthinking/nami/master/nami.js";
 import { join } from "https://deno.land/std@0.126.0/path/mod.ts";
 
-var n = nami("ipio");
-n.not_open_sourced_on("https://github.com/txthinking/ipio");
+var n = nami("tun2brook");
+n.not_open_sourced_on("https://github.com/txthinking/tun2brook");
 
-var r = await n.fetch("https://api.github.com/repos/txthinking/ipio/releases/latest");
+var r = await n.fetch("https://api.github.com/repos/txthinking/tun2brook/releases/latest");
 var j = await r.json();
 await n.version(j.tag_name);
 
 if (Deno.build.os == "darwin" && Deno.build.arch == "aarch64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/ipio_darwin_arm64", "ipio");
+    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_darwin_arm64", "tun2brook");
     Deno.exit(0);
 }
 if (Deno.build.os == "darwin" && Deno.build.arch == "x86_64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/ipio_darwin_amd64", "ipio");
+    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_darwin_amd64", "tun2brook");
     Deno.exit(0);
 }
 if (Deno.build.os == "linux" && Deno.build.arch == "x86_64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/ipio_linux_amd64", "ipio");
+    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_linux_amd64", "tun2brook");
     Deno.exit(0);
 }
 if (Deno.build.os == "windows" && Deno.build.arch == "x86_64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/ipio_windows_amd64.exe", "ipio.exe");
+    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_windows_amd64.exe", "tun2brook.exe");
     try{
         await Deno.remove(join(Deno.env.get("HOME"), ".nami", "bin", "wintun.dll"));
         await Deno.remove(join(Deno.env.get("HOME"), ".nami", "bin", "wintun.h"));
