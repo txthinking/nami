70a604998b671cf6d20785b5c291795ba9452422
TxThinking <cloud@txthinking.com>
2022-04-13 17:02:53 +0800

    Update tun2brook.js

--- package/tun2brook.js
+++ package/tun2brook.js
@@ -9,19 +9,19 @@ var j = await r.json();
 await n.version(j.tag_name);
 
 if (Deno.build.os == "darwin" && Deno.build.arch == "aarch64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_darwin_arm64", "tun2brook");
+    await n.download_command_from_url("https://github.com/txthinking/tun2brook/releases/latest/download/tun2brook_darwin_arm64", "tun2brook");
     Deno.exit(0);
 }
 if (Deno.build.os == "darwin" && Deno.build.arch == "x86_64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_darwin_amd64", "tun2brook");
+    await n.download_command_from_url("https://github.com/txthinking/tun2brook/releases/latest/download/tun2brook_darwin_amd64", "tun2brook");
     Deno.exit(0);
 }
 if (Deno.build.os == "linux" && Deno.build.arch == "x86_64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_linux_amd64", "tun2brook");
+    await n.download_command_from_url("https://github.com/txthinking/tun2brook/releases/latest/download/tun2brook_linux_amd64", "tun2brook");
     Deno.exit(0);
 }
 if (Deno.build.os == "windows" && Deno.build.arch == "x86_64") {
-    await n.download_command_from_url("https://github.com/txthinking/ipio/releases/latest/download/tun2brook_windows_amd64.exe", "tun2brook.exe");
+    await n.download_command_from_url("https://github.com/txthinking/tun2brook/releases/latest/download/tun2brook_windows_amd64.exe", "tun2brook.exe");
     try{
         await Deno.remove(join(Deno.env.get("HOME"), ".nami", "bin", "wintun.dll"));
         await Deno.remove(join(Deno.env.get("HOME"), ".nami", "bin", "wintun.h"));
