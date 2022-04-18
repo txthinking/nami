3d61e1b7180366ab4e0d762654fedc59a2ec91c9
TxThinking <cloud@txthinking.com>
2022-04-18 17:10:02 +0800

    Create push.sh

---
+++ package/push.sh
@@ -0,0 +1,7 @@
+import nami from 'https://raw.githubusercontent.com/txthinking/nami/master/nami.js';
+import { join } from "https://deno.land/std@0.126.0/path/mod.ts";
+
+var n = nami("push");
+n.open_sourced_on("https://github.com/txthinking/push");
+
+await Deno.writeFile(`${join(Deno.env.get("HOME"), ".nami", "cache", "push")}`, new TextEncoder().encode('git add . && git commit -m null && git push origin master'));
