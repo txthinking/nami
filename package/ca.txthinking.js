cf029c661ccbff31958c7d8714166be60459ec06
TxThinking <cloud@txthinking.com>
2022-04-29 15:11:06 +0800

    Create ca.txthinking.js

---
+++ package/ca.txthinking.js
@@ -0,0 +1,8 @@
+import nami from "https://raw.githubusercontent.com/txthinking/nami/master/nami.js";
+import {sh, s2b, home, echo} from 'https://raw.githubusercontent.com/txthinking/denolib/master/f.js';
+
+var n = nami("ca.txthinking");
+n.open_sourced_on("https://github.com/txthinking/ca");
+
+await n.download_command_from_url("https://txthinking.github.io/ca/ca.pem", "ca.pem");
+await sh(`mad install --ca ${home(".nami", "bin", "ca.pem")}`);
