e2d5f4d39a8f4b209f359d7e459c3dbef65c3866
TxThinking <cloud@txthinking.com>
2023-06-22 20:50:07 +0800

    Create monitor.js

---
+++ package/monitor.js
@@ -0,0 +1,14 @@
+// check os
+if(!(nami.os == "linux" && nami.arch == "amd64") &&
+    !(nami.os == "linux" && nami.arch == "arm64")){
+    echo("This package does not support your OS or Arch now. PR welcome: https://github.com/txthinking/nami/blob/master/package")
+    exit(1)
+}
+
+// version
+var s = $1("curl -L https://api.github.com/repos/txthinking/bash/releases/latest")
+var v = JSON.parse(s).tag_name
+await writefile(`${nami.cache_dir}/version`, v)
+
+// download and copy command files into nami.cache_dir
+cp(`https://github.com/txthinking/bash/releases/latest/download/monitor_${nami.os}_${nami.arch}${nami.os == "windows" ? '.exe' : ''}`, `${nami.cache_dir}/monitor${nami.os == "windows" ? '.exe' : ''}`)
