fe3c6f263579e16e16cb870011186379ca2a4c49
txthinking <cloud@txthinking.com>
2024-04-08 22:02:47 +0800

    build

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ fi
 
 mkdir _
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-w -s" -o _/nami_darwin_arm64
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_darwin_amd64
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_linux_amd64
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o _/nami_linux_arm64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o _/nami_windows_amd64.exe
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build  -trimpath -ldflags="-w -s" -o _/nami_darwin_arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -trimpath -ldflags="-w -s" -o _/nami_darwin_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build   -trimpath -ldflags="-w -s" -o _/nami_linux_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build   -trimpath -ldflags="-w -s" -o _/nami_linux_arm64
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -trimpath -ldflags="-w -s" -o _/nami_windows_amd64.exe
 
 nami release github.com/txthinking/nami $1 _
 
